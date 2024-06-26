3f51cc2bce477b0647025995412e2ba30de63ace
JeanLeGenou <122476334+jdboud@users.noreply.github.com>
2024-06-26 08:00:58 -0300

    up ng

--- static/network-graph.js
+++ static/network-graph.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function fetchData() {
-        fetch('https://jdboud.github.io/SRM/data/binaryCleanUserNumberCollections1Test024.xlsx')
+        fetch('data/')
             .then(response => response.json())
             .then(data => {
                 graphData = data;
