98a25409b152ee66990087f43b92ad6c46ef7618
JeanLeGenou <122476334+jdboud@users.noreply.github.com>
2024-06-27 11:31:27 -0300

    P

--- static/network-graph.js
+++ static/network-graph.js
@@ -518,5 +518,9 @@ document.addEventListener('DOMContentLoaded', function() {
         legend.exit().remove();
     }
 
+    function openNodeDetails(d) {
+        console.log("Node details:", d);
+    }
+
     fetchData();
 });
